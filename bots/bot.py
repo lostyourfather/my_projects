37686c043225534fa174cf2101c3bbd167e80aa3
Artem <rodinart52@gmail.com>
2020-03-31 01:15:29 +0300

    refactoring

--- bots/bot.py
+++ bots/bot.py
@@ -3,6 +3,8 @@ import pyowm
 
 bot = telebot.TeleBot("1087470565:AAHKtxzBruFyG2uQDb5cv97uS-5lHvJohSs")
 owm = pyowm.OWM('55dc960512c3a0f6ce8417a61cabd7b8', language="en")
+
+
 @bot.message_handler(content_types=["text"])
 def send_welcome(message):
     try:
@@ -11,11 +13,12 @@ def send_welcome(message):
         windSpeed = w.get_wind()
         humidity = w.get_humidity()
         status = w.get_detailed_status()
-        answer = "In city " + message.text + " " + str(w.get_temperature("celsius")["temp"]) + " degree " + "status: " + str(status) + " speed of wind: " + str(windSpeed["speed"]) + " m/s " + "humidity: " + str(humidity)
+        answer = "In city " + message.text + " " + str(
+            w.get_temperature("celsius")["temp"]) + " degree " + "status: " + str(status) + " speed of wind: " + str(
+            windSpeed["speed"]) + " m/s " + "humidity: " + str(humidity)
         bot.send_message(message.chat.id, answer)
     except:
         bot.send_message(message.chat.id, "I don't know this city")
-
 
 
 bot.polling(none_stop=True)
